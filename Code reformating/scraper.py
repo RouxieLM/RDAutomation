7261aa3bd37f64f984506115a543a8e6a9ceff69
Lucas MATHIEU <lmathieu76@outlook.fr>
2023-09-07 04:58:27 +0200

    [CodeReformat, scraper.py] return magnet value

--- Code reformating/scraper.py
+++ Code reformating/scraper.py
@@ -71,3 +71,5 @@ def scraper():
     encoded_dn = urllib.parse.quote(selected_name)
 
     magnet = f"magnet:?xt=urn:btih:{selected_thash}&dn={encoded_dn}&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.bittor.pw%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=udp%3A%2F%2Fbt.xxx-tracker.com%3A2710%2Fannounce&tr=udp%3A%2F%2Fpublic.popcorn-tracker.org%3A6969%2Fannounce&tr=udp%3A%2F%2Feddie4.nl%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.torrent.eu.org%3A451%2Fannounce&tr=udp%3A%2F%2Fp4p.arenabg.com%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.tiny-vps.com%3A6969%2Fannounce&tr=udp%3A%2F%2Fopen.stealth.si%3A80%2Fannounce"
+
+    return magnet
