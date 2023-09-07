83d181f7bf16e195e2f255734042ef78540f33a7
Lucas MATHIEU <lmathieu76@outlook.fr>
2023-09-07 05:20:16 +0200

    [CodeReformat, download.py] implemented check_availability()

--- Code reformating/download.py
+++ Code reformating/download.py
@@ -3,13 +3,28 @@ import shutil
 import os
 import urllib.parse
 from urllib.parse import urlparse
+import json
+
+url = "https://api.real-debrid.com/rest/1.0"
+
+def check_availability(rd_api, selected_thash):
+    header = {"Authorization": f"Bearer {rd_api}"}
+    instantAvailability = "/torrents/instantAvailability/"
+
+    ia = requests.get(url+instantAvailability+selected_thash, headers=header)
+
+    json_ia = json.loads(ia.text)
+
+    if len(json_ia[f'{selected_thash}'.lower()]) == 0:
+        print('The torrent file is not available on Real Debrid, this application is not able (yet) to wait for Real Debrid to convert the magnet to a torrent file and download its content. Please add it manually via the web interface and try again. Note that it is a rare occurence, RD has thousands of torrents cached.')
+        exit(0)
+    else:
+        print('Torrent available on Real Debrid, proceeding to download...')
 
 
 def download(rd_api, magnet, season_path):
-    url = "https://api.real-debrid.com/rest/1.0"
     header = {"Authorization": f"Bearer {rd_api}"}
     torrents = "/torrents"
-    torrents_info = "/torrents/info/"
     unrestrict = "/unrestrict/link"
     addMagnet = "/torrents/addMagnet"
     data_magnet = {"magnet": magnet}
@@ -20,7 +35,7 @@ def download(rd_api, magnet, season_path):
     post_magnet_json = post_magnet.json()
     tid = post_magnet_json['id']
 
-    select_files = requests.post(url+selectFiles+tid, headers=header, data=data_select_files)
+    requests.post(url+selectFiles+tid, headers=header, data=data_select_files)
 
     torrent = requests.get(url+torrents, headers=header)
     torrent_json = torrent.json()
