f61336283e51f652bab8f8c4254f7281763662b0
Lucas MATHIEU <lmathieu76@outlook.fr>
2023-10-29 12:07:23 +0100

    [checkAvailability.py, miscs] Avoiding the brutal end of the script, we now wait for user input to exit

--- checkAvailability.py
+++ checkAvailability.py
@@ -12,6 +12,9 @@ def check_availability(rd_api, selected_thash):
 
     if len(json_ia[f'{selected_thash}'.lower()]) == 0:
         print('The torrent file is not available on Real Debrid, this application is not able (yet) to wait for Real Debrid to convert the magnet to a torrent file and download its content. Please add it manually via the web interface and try again. Note that it is a rare occurence, RD has thousands of torrents cached.')
-        exit(0)
+        while True:
+            user_input = input("Press any key to exit.")
+            if user_input == "":
+                exit(0)
     else:
         print('Torrent available on Real Debrid, proceeding to download...')
